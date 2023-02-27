306a4cf2e2a9c16533fc655bc7b560c8263f6ae8
josenildolins <josenildo.lins@gmail.com>
2023-02-27 17:42:12 -0300

    feat: excluindo os comentarios da classe Nokia

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,6 +1,5 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
     public class Nokia : Smartphone
     {
         public Nokia(string numero, string modelo, string iMEI, int memoria) 
@@ -8,7 +7,6 @@ namespace DesafioPOO.Models
         {
             
         }
-        // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"Instalando  Aplicativo no celular: {Modelo}");
